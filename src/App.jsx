f95f8b609c82f5d0a90319013875a31fd079bb69
AMANverma9118 <AMANverma9118359330@gmail.com>
2024-06-12 00:44:20 +0530

    When scrolling the color og navbar changes successfully

--- src/App.jsx
+++ src/App.jsx
@@ -3,17 +3,20 @@ import Navbar from './Components/Navbar/Navbar'
 import Hero from './Components/Hero/Hero'
 import Programs from './Components/Programs/Programs'
 import Title from './Components/Title/Title'
+import About from './Components/About/About'
 
 
 
 const App = () => {
   return (
     <div>
-      <Navbar/>
-      <Hero/>
+      <Navbar />
+      <Hero />
       <div className="container">
-        <Title subtitle="OUR PROGRAM" title="What We Offer"/>
-      <Programs/>
+        <Title subtitle="OUR PROGRAM" title="What We Offer" />
+        <Programs />
+        <About/>
+        <Title subtitle="Gallery" title="Campus photos" />
       </div>
     </div>
   )
