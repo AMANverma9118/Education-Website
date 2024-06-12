0f732e218560e7eaab5afd8234892a8056bb0476
AMANverma9118 <AMANverma9118359330@gmail.com>
2024-06-12 15:59:28 +0530

    Campus is ready

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Hero from './Components/Hero/Hero'
 import Programs from './Components/Programs/Programs'
 import Title from './Components/Title/Title'
 import About from './Components/About/About'
+import Campus from './Components/Campus/Campus'
 
 
 
@@ -17,6 +18,7 @@ const App = () => {
         <Programs />
         <About/>
         <Title subtitle="Gallery" title="Campus photos" />
+        <Campus/>
       </div>
     </div>
   )
